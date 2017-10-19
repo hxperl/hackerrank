59b9375e97753bfa21226f437080adc11f9002eb
hxperl <hxperl@gmail.com>
2017-10-19 15:01:15 +0900

    Nested list

---
+++ python3/BasicDataTypes/NestedList.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    students=list()
+    scores = list()
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        scores.append(score)
+        students.append([name,score])
+    minimun = min(scores)
+    scores = set(scores)
+    scores.remove(minimun)
+    second_minimum = min(scores)
+    print(*[i[0] for i in sorted(students) if i[1]== second_minimum], sep="\n")
+
+
