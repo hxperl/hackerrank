0d01dac8e50425fab82e9bc5c5d446bb5f8b4561
hxperl <hxperl@gmail.com>
2017-10-20 17:25:52 +0900

    list to set

--- python3/Strings/FindAString.py
+++ python3/Strings/FindAString.py
@@ -1,7 +1,7 @@
 def count_substring(string, sub_string):
-    tmp=list()
+    tmp = set()
     for i in range(len(string)):
         index = string.find(sub_string, i)
-        if index not in tmp and index != -1:
-            tmp.append(index)
+        if index != -1:
+            tmp.add(index)
     return len(tmp)
