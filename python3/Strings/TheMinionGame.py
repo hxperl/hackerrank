931cc91131eed1f34afedd7c95a1992b66c5021a
hxperl <hxperl@gmail.com>
2017-10-25 16:38:17 +0900

    Update: add length of substrings

--- python3/Strings/TheMinionGame.py
+++ python3/Strings/TheMinionGame.py
@@ -1,43 +1,20 @@
-def count_substring(string, sub_string):
-    tmp = set()
-    for i in range(len(string)):
-        index = string.find(sub_string, i)
-        if index != -1:
-            tmp.add(index)
-    return len(tmp)
-
-def _(string, v_flag=False):
-    name = dict()
-    vowels = "AEIOU"
-    tmp = ""
-    for c, i in enumerate(string):
-        if v_flag:
-            if i in vowels and i not in name.keys():
-                name[i]=count_substring(string, i)
-                tmp = string[c]
-                for j in range(c+1, len(string)):
-                    tmp = tmp+string[j]
-                    if tmp not in name.keys():
-                        name[tmp] = count_substring(string, tmp)
-        else:
-            if i not in vowels and i not in name.keys():
-                name[i]=count_substring(string, i)
-                tmp = string[c]
-                for j in range(c+1, len(string)):
-                    tmp = tmp+string[j]
-                    if tmp not in name.keys():
-                        name[tmp] = count_substring(string, tmp)
-    return name
+from collections import defaultdict
 
 def minion_game(string):
-    stuart = _(string, v_flag=False)
-    kevin = _(string, v_flag=True)
-    st = sum(stuart.values())
-    kv = sum(kevin.values())
-    if st > kv:
-        print("Stuart "+str(st))
-    elif kv > st:
-        print("Kevin "+str(kv))
+    vowels = 'AEIOU'
+
+    kevsc = 0
+    stusc = 0
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevsc += (len(string)-i)
+        else:
+            stusc += (len(string)-i)
+
+    if kevsc > stusc:
+        print("Kevin", kevsc)
+    elif kevsc < stusc:
+        print("Stuart", stusc)
     else:
         print("Draw")
 if __name__=='__main__':
