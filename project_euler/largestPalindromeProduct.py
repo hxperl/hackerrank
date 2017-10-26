d12bd5aefaeed0143d2dda749e8e953621f0391b
hxperl <hxperl@gmail.com>
2017-10-26 19:04:12 +0900

    Add : Largest palindrome product - failed testcase 3

---
+++ project_euler/largestPalindromeProduct.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import sys
+
+def check_product(number):
+    for i in range(100, 999):
+        z = int(number/i)
+        if number % i == 0 and 99 < z and z < 1000:
+            return True
+    return False
+    
+def check_palindrome(number):
+    if str(number) == ''.join(reversed(str(number))):
+        return True
+    else:
+        return False
+
+t = int(input().strip())
+tmp = list()
+for a0 in range(t):
+    n = int(input().strip())
+    for i in range(n, 0, -1):
+        if check_palindrome(i) and check_product(i):
+            tmp.append(i)
+            break
+print(*tmp, sep="\n")
+
