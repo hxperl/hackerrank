75e746800b0d715fdd5b0eab0babd279e0282a9f
hxperl <hxperl@gmail.com>
2017-10-30 17:45:23 +0900

    Update : Largest Prime factor clear

--- project_euler/largestPrimeFactor.py
+++ project_euler/largestPrimeFactor.py
@@ -1,22 +1,45 @@
 #!/bin/python3
 
 import sys
+import math
 
-def check_prime(number):
-    if number > 1:
-        for i in range(2, number):
-            if number % i ==0:
-                return False
-        else:
-            return True
+def isprime(n):
+    """Returns True if n is prime."""
+    if n == 2:
+        return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
 
+    i = 5
+    w = 2
+
+    while i * i <= n:
+        if n % i == 0:
+            return False
+
+        i += w
+        w = 6 - w
+
+    return True
 t = int(input().strip())
 tmp = list()
 for a0 in range(t):
     n = int(input().strip())
-    for i in range(n, 0, -1):
-        if n % i == 0 and check_prime(i):
-            tmp.append(i)
-            break
+    primes=list()
+    for i in range(1, int(math.sqrt(n))+1):
+        if n % i == 0:
+            res = n // i
+            if isprime(res):
+                primes.append(res)
+            if isprime(i):
+                primes.append(i)
+    tmp.append(max(primes))
 print(*tmp, sep="\n")
-        
+
+
+
+
