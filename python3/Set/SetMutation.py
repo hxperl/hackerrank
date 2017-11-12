c73b28534763aaf263ad54d4d62a89ba420ae36f
hxperl <hxperl@gmail.com>
2017-11-12 17:03:54 +0900

    Add : Set Add Mutation update .py - pass

--- python3/Set/SetMutation.py
+++ python3/Set/SetMutation.py
@@ -1,2 +1,18 @@
 a = int(input())
-s = set(map(int, input().split()))
+set_a = set(map(int, input().split()))
+b = int(input())
+
+for _ in range(b):
+    read = input().split()
+    cmd = read[0]
+    new_set = set(map(int,input().split()))
+    if cmd == 'intersection_update':
+        set_a.intersection_update(new_set)
+    elif cmd == 'update':
+        set_a.update(new_set)
+    elif cmd == 'symmetric_difference_update':
+        set_a.symmetric_difference_update(new_set)
+    elif cmd == 'difference_update':
+        set_a.difference_update(new_set)
+
+print(sum(set_a))
