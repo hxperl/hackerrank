a74d513fd133b58b89e4d26c81c924f21ad87e0b
hxperl <hxperl@gmail.com>
2020-04-10 14:29:19 +0900

    refactor

--- InterviewPreparationKit/DictionariesAndHashmaps/HashTables:RansomNote.swift
+++ InterviewPreparationKit/DictionariesAndHashmaps/HashTables:RansomNote.swift
@@ -19,29 +19,27 @@ func checkMagazine(magazine: [String], note: [String]) -> Void {
 	print("Yes")
 }
 
-checkMagazine(magazine: ["two", "times", "three", "is", "four"], note: ["two", "times", "two", "is", "four"])
+guard let mnTemp = readLine() else { fatalError("Bad input") }
+let mn = mnTemp.split(separator: " ").map{ String($0) }
 
-// guard let mnTemp = readLine() else { fatalError("Bad input") }
-// let mn = mnTemp.split(separator: " ").map{ String($0) }
+guard let m = Int(mn[0].trimmingCharacters(in: .whitespacesAndNewlines))
+else { fatalError("Bad input") }
 
-// guard let m = Int(mn[0].trimmingCharacters(in: .whitespacesAndNewlines))
-// else { fatalError("Bad input") }
+guard let n = Int(mn[1].trimmingCharacters(in: .whitespacesAndNewlines))
+else { fatalError("Bad input") }
 
-// guard let n = Int(mn[1].trimmingCharacters(in: .whitespacesAndNewlines))
-// else { fatalError("Bad input") }
+guard let magazineTemp = readLine() else { fatalError("Bad input") }
+let magazine: [String] = magazineTemp.split(separator: " ").map {
+    String($0)
+}
 
-// guard let magazineTemp = readLine() else { fatalError("Bad input") }
-// let magazine: [String] = magazineTemp.split(separator: " ").map {
-//     String($0)
-// }
+guard magazine.count == m else { fatalError("Bad input") }
 
-// guard magazine.count == m else { fatalError("Bad input") }
+guard let noteTemp = readLine() else { fatalError("Bad input") }
+let note: [String] = noteTemp.split(separator: " ").map {
+    String($0)
+}
 
-// guard let noteTemp = readLine() else { fatalError("Bad input") }
-// let note: [String] = noteTemp.split(separator: " ").map {
-//     String($0)
-// }
+guard note.count == n else { fatalError("Bad input") }
 
-// guard note.count == n else { fatalError("Bad input") }
-
-// checkMagazine(magazine: magazine, note: note)
+checkMagazine(magazine: magazine, note: note)
